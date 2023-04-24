a14b7064933c11163764d3c92921f10906e3f927
packertest <106478344+packertest@users.noreply.github.com>
2023-04-25 07:24:21 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,67 +1,20 @@
-import sqlite3
-import os
-
-
-conn = sqlite3.connect('files.db')
-cursor = conn.cursor()
-
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS files
-    (id INTEGER PRIMARY KEY AUTOINCREMENT,
-    filename TEXT,
-    filetype TEXT,
-    filepath TEXT,
-    filesize INTEGER,
-    filecontent BLOB)
-    ''')
-
-def insert_file(filename, filetype, filepath, filesize, filecontent):
-    cursor.execute('''
-        INSERT INTO files (filename, filetype, filepath, filesize, filecontent)
-        VALUES (?, ?, ?, ?, ?)''', (filename, filetype, filepath, filesize, filecontent))
-    conn.commit()
-    print('insert ok')
-
-
-def select_file(filename):
-    obj = cursor.execute("SELECT * FROM files WHERE filename=?", (filename,))
-    # result = cursor.fetchone()
-    for result in obj:
-        if result:
-            print(f"ID：{result[0]}")
-            print(f"File Name：{result[1]}")
-            print(f"File Type：{result[2]}")
-            print(f"File Path：{result[3]}")
-            print(f"File Size：{result[4]}")
-            print(f"File Content：{result[5]}")
-        else:
-            print("record does not exist")
-
-
-def update_file(filename, new_filepath):
-    cursor.execute("UPDATE files SET filepath=? WHERE filename=?", (new_filepath, filename))
-    conn.commit()
-    print('updated ok')
-
-
-def delete_file(filename):
-    cursor.execute("DELETE FROM files WHERE filename=?", (filename,))
-    conn.commit()
-    print('delete ok')
-
-
-with open('test.txt', 'rb') as f:
-    content = f.read()
-insert_file('test.txt', 'txt', os.path.abspath('test.txt'), os.path.getsize('test.txt'), content)
-
-
-select_file('test.txt')
-
-
-update_file('test.txt', os.path.abspath('test/test.txt'))
-
-
-# delete_file('test.txt')
-
-cursor.close()
-conn.close()
+with open(r'D:\Codes\test\eml\test doc.eml', 'rb') as f:
+    msg = email.message_from_binary_file(f)
+    
+# 提取附件并保存到指定文件夹
+if msg.is_multipart():
+    for part in msg.walk():
+        content_type = part.get_content_type()
+        disposition = str(part.get("Content-Disposition"))
+        if content_type != 'text/plain' and 'attachment' in disposition:
+            filename = part.get_filename()
+            if filename:
+                # 解码文件名
+                decoded_filename = decode_header(filename)[0][0]
+                if isinstance(decoded_filename, bytes):
+                    # 如果文件名是bytes类型，则解码为字符串
+                    decoded_filename = decoded_filename.decode()
+                # 拼接文件路径并保存文件
+                filepath = os.path.join(r'/home/test', decoded_filename)
+                with open(filepath, 'wb') as f:
+                    f.write(part.get_payload(decode=True))
