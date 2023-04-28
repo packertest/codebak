709cc30eac01ab6db346f0bf473a76b9b0411afc
packertest <106478344+packertest@users.noreply.github.com>
2023-04-28 08:02:20 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,38 +1,5 @@
-import zipfile
-
-with zipfile.ZipFile("example.zip", "r") as zip_ref:
-    zip_ref.extractall("extracted_folder")
-##################################################
-import tarfile
-
-with tarfile.open("example.tar.gz", "r:gz") as tar_ref:
-    tar_ref.extractall("extracted_folder")
-
-##################################################
-import gzip
-
-with gzip.open("example.gz", "rb") as gz_ref:
-    with open("example", "wb") as file_ref:
-        file_ref.write(gz_ref.read())
-##################################################
-import bz2
-
-with bz2.BZ2File("example.bz2", "rb") as bz2_ref:
-    with open("example", "wb") as file_ref:
-        file_ref.write(bz2_ref.read())
-##################################################
-
-import tarfile
-
-with tarfile.open("example.tar.gz", "r:gz") as tar_ref:
-    tar_ref.extractall("extracted_folder")
-
-import patoolib
-
-patoolib.extract_archive("example.rar", outdir="extracted_folder")
-
-
-import py7zr
-
-with py7zr.SevenZipFile('example.7z', mode='r') as archive:
-    archive.extractall(path='extracted_folder')
+U2FsdGVkX1+xOOROrGh1lnDEUwnQ3QIzNakjE+uCxT5CpxK3bgg9FyN1DuKtB0ZF
+SsDyioOnTN5X9y/XeOEQw+dfEYkfNaskRa4UpC4sv9E4EzIOtedipTdUISu+huyZ
+SLeZEOBAlPXQRMkZMvyJ6HKHF9q2mq5nQ5TXIdIA/5GLutRpbtf06EUc847C8Q/I
+pYnHt+Uq2Nnav1PWc5UwNkZn7mO2AR0UGwxtWHIW0+w=
+https://tool.oschina.net/encrypt/
