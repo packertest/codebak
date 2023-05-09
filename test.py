2e00a59f212a70e91ce57cf83539bc483be1c691
packertest <106478344+packertest@users.noreply.github.com>
2023-05-10 07:46:26 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-
-https://www.bilibili.com/read/mobile?id=18259851
+https://blog.csdn.net/u012822181/article/details/107267589
+error while loading shared libraries: libcrypto.so.3: cannot open shared object file: No such file or directory
+      https://askubuntu.com/questions/1462100/error-while-loading-shared-libraries-libcrypto-so-3-ubuntu-20-04
