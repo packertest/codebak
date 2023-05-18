08153b2a1474363f5ceacb1dd3063bd144deafa6
packertest <106478344+packertest@users.noreply.github.com>
2023-05-18 22:47:45 +0800

    Update and rename test.py to test.txt

--- test.py
+++
@@ -1,3 +0,0 @@
-https://blog.csdn.net/u012822181/article/details/107267589
-error while loading shared libraries: libcrypto.so.3: cannot open shared object file: No such file or directory
-      https://askubuntu.com/questions/1462100/error-while-loading-shared-libraries-libcrypto-so-3-ubuntu-20-04
